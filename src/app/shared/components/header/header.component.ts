46825c37d4ec13b98100f61444d7a1d84f939542
José Luis Zúñiga <jose@tangibledesign.co>
2024-03-12 17:09:02 -0500

    Se agrega control para mostrar el menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  logo!: string;
+  menu!: string;
+  ngOnInit() {
+    this.setImageBasedOnScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.setImageBasedOnScreenSize();
+  }
+
+  private setImageBasedOnScreenSize() {
+    const screenWidth = window.innerWidth;
+    // Lógica para determinar la ruta de la imagen según el tamaño de la pantalla
+    if (screenWidth < 768) {
+      console.log("Menos de 768px")
+      this.logo = '../../../../assets/global/Logo-sm.svg';
+      this.menu = '../../../../assets/global/menu-sm.png';
+    } else {
+      this.logo = '../../../../assets/global/Logo-lg.svg';
+      this.menu = '../../../../assets/global/menu-lg.svg';
+    }
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule
