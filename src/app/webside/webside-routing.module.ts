782091af0962761fb78a17c50897b5455055380a
José Luis Zúñiga <jose@tangibledesign.co>
2024-04-16 12:34:36 -0500

    Cambio al path principal de la pagina

--- src/app/webside/webside-routing.module.ts
+++ src/app/webside/webside-routing.module.ts
@@ -8,13 +8,13 @@ const routes: Routes = [
     path: '',
     component: LayoutComponent,
     children:[
+      // {
+      //   path: '',
+      //   pathMatch: 'full',
+      //   redirectTo: 'home'
+      // },
       {
         path: '',
-        pathMatch: 'full',
-        redirectTo: 'home'
-      },
-      {
-        path: 'home',
         component: HomeComponent
       }
     ]
