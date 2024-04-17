3a10f4fa751c1424c2ff9017d5be79b0b331d609
José Luis Zúñiga <jose@tangibledesign.co>
2024-04-17 13:59:11 -0500

    Se agrega navegación y redes en el menu

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter,  } from '@angular/core';
+import { Component, Input, Output, EventEmitter, Inject,  } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -9,8 +10,39 @@ export class MenuComponent {
   @Input() active: boolean = false;
   @Output() activeStateChange = new EventEmitter<boolean>()
 
-  changeHiddenMenu(){
+  copyMail: boolean = false;
+
+  constructor (
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+
+
+  toogleMenu(){
     this.active = !this.active;
     this.activeStateChange.emit(this.active);
+  }
+
+  public goto(page: string) {
+    this.document?.getElementById(page)?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    })
+    this.toogleMenu();
+  }
+
+  copyToClipboard(value: string): void {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = value;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
   }
 }
