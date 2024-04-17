d829b7168b0d018ab0b9c6bffcc8a6715bdf38a7
José Luis Zúñiga <jose@tangibledesign.co>
2024-04-17 10:13:31 -0500

    Se elimina bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
